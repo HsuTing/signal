c4cd6c05ea8060df45d40d5f7d9b287a832e91d5
HsuTing <hsuting0106@gmail.com>
2016-03-27 23:28:58 +0800

    lab3 final code

--- lab3/READNE.md
+++ lab3/READNE.md
@@ -1,3 +1,4 @@
 # Lab 3
 
 Input an audio file and use `Convolution` to process the file.
+Not using matlab function `conv`, this project just use for to make same effect as `conv`.
--- lab3/main.m
+++ lab3/main.m
@@ -1,10 +1,29 @@
 clear all;
 
-[data, fs] = audioread('arena.wav');
+[xIn, xfs] = audioread('cat.wav', [1, 16963]);
+[hIn, hfs] = audioread('arena.wav', [1, 16963]);
 
-output = conv(data, fs);
+x = xIn(:, 1);
+h = hIn(:, 1);
 
-subplot(2,1,1); plot(data); title('input');
-subplot(2,1,2); plot(output); title('output');
+n = length(x);
+k = length(h);
+
+x1 = [x, zeros(k, 1)];
+h1 = [h, zeros(n, 1)];
+for i = 1:n + k -1
+     y(i) = 0;
+     for j = 1:n
+         if(i - j + 1 > 0)
+             y(i) = y(i) + x1(j) * h1(i - j + 1); 
+             m(i) = y(i);
+         else
+         end 
+     end
+end
+
+for i = 1: n
+    output(i) = m(i);
+end
 
 sound(output);
